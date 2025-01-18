8790ae2fce75998bbf961661ac26e85bfa8d51c0
Mateusz Pawlak <pawlakmateusz96@gmail.com>
2025-01-18 22:12:57 +0100

    add model for dish

--- catalog/models.py
+++ catalog/models.py
@@ -9,3 +9,16 @@ class DishType(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(decimal_places=2, max_digits=10)
+    dish_type = models.ForeignKey(DishType, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
