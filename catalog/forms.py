a89ec6023993fe7f0420c9fa864c59f345da59b2
Mateusz Pawlak <pawlakmateusz96@gmail.com>
2025-01-20 21:05:31 +0100

    add validation for experience update

--- catalog/forms.py
+++ catalog/forms.py
@@ -8,6 +8,14 @@ class CookExperienceUpdateForm(forms.ModelForm):
         model = Cook
         fields = ["years_of_experience"]
 
+    def clean_years_of_experience(self):
+        return validate_years_of_experience(self.cleaned_data["years_of_experience"])
+
+def validate_years_of_experience(value):
+    if value < 0:
+        raise forms.ValidationError("Years cannot be negative")
+    return value
+
 
 class CookSearchForm(forms.Form):
     username = forms.CharField(
