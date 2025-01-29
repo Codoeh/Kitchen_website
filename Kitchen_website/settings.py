233faf5358991ebe8323aec6d32a8e2e09a8fdf0
Mateusz Pawlak <pawlakmateusz96@gmail.com>
2025-01-29 10:22:54 +0100

    update settings/ add requirements.txt

--- Kitchen_website/settings.py
+++ Kitchen_website/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-%w+z^8r__e*!fyy1@w0frsy0d#sy8n(n2o8us=+d$o96tfa9@@"
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-%w+z^8r__e*!fyy1@w0frsy0d#sy8n(n2o8us=+d$o96tfa9@@")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 
 
 # Application definition
