a887e2bd2d6e03b10167651f6cc561cd6bf2befb
Mateusz Pawlak <pawlakmateusz96@gmail.com>
2025-11-26 00:37:29 +0100

    Add test user

--- Kitchen_website/wsgi.py
+++ Kitchen_website/wsgi.py
@@ -14,28 +14,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Kitchen_website.settings")
 
 application = get_wsgi_application()
-
-
-import os
-from django.contrib.auth import get_user_model
-from django.db.utils import OperationalError
-
-TEST_USER = os.environ.get("TEST_USER_NAME")
-TEST_PASS = os.environ.get("TEST_USER_PASSWORD")
-TEST_EMAIL = os.environ.get("TEST_USER_EMAIL")
-
-if TEST_USER and TEST_PASS:
-    try:
-        User = get_user_model()
-
-        if not User.objects.filter(username=TEST_USER).exists():
-            User.objects.create_user(
-                username=TEST_USER,
-                email=TEST_EMAIL or "",
-                password=TEST_PASS,
-            )
-            print(">>> [Render] Created test user:", TEST_USER)
-        else:
-            print(">>> [Render] Test user already exists.")
-    except OperationalError:
-        print(">>> [Render] Database not ready. Skipping test user creation.")
--- build.sh
+++ build.sh
@@ -10,3 +10,7 @@ python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
 python manage.py migrate
+
+python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model();
+User.objects.filter(username='$TEST_USER_NAME').exists() or User.objects.create_superuser(
+'$TEST_USER_NAME', '$TEST_USER_EMAIL', '$TEST_USER_PASSWORD')"
